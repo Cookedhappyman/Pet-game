ba6dda0bd32d37ebdd970ad950514dece3c002c1
Cookedhappyman <146384471+Cookedhappyman@users.noreply.github.com>
2023-12-07 18:25:28 +0000

    First commit

---
+++ Pet.py
@@ -0,0 +1,26 @@
+from Zodiac_Sign import Zodiac
+
+#this will contain the 
+universal_time = 0
+
+class Pet:
+
+    def __init__(self):
+        self.mental_health = 0
+        self.physical_health = 0
+        self.IQ = 0
+        self.hunger = 0
+        self.cleanliness = 0
+        self.attraction = 0
+        self.wildness = 0
+        self.age = 0
+        self.weight = 0
+        self.trust = 0
+
+    #============================================================
+    #calculations
+    #============================================================
+
+    #calculates age of the pet
+    def __calc_age(self):
+        universal_time
---
+++ Zodiac_Sign.py
@@ -0,0 +1,12 @@
+
+
+class Zodiac:
+    
+    def __init__(self):
+        self.mental_health_rate = 0
+        self.physical_health = 0
+        self.hunger_rate = 0
+        self.cleanliness_rate = 0
+        self.attractiveness = 0
+        self.trained_rate = 0
+    
